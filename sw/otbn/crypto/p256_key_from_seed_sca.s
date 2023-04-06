b137a5034718ae3aa4a0a7222419e307e60cb0a0
Abdullah Varici <abdullah.varici@lowrisc.org>
2023-04-06 14:27:51 +0100

    Original ECC keygen program.

--- sw/otbn/crypto/p256_key_from_seed_sca.s
+++ sw/otbn/crypto/p256_key_from_seed_sca.s
@@ -69,18 +69,18 @@ run_gen_secret_key:
        [w23,w33] <= d1 */
   jal       x1, p256_key_from_seed
 
-#  /* Write the results to DMEM.
-#       dmem[d0] <= [w21, w20]
-#       dmem[d1] <= [w23, w22] */
-#  li        x2, 20
-#  la        x3, d0
-#  bn.sid    x2, 0(x3++)
-#  li        x2, 21
-#  bn.sid    x2++, 0(x3)
-#  la        x3, d1
-#  bn.sid    x2, 0(x3++)
-#  li        x2, 23
-#  bn.sid    x2, 0(x3)
+  /* Write the results to DMEM.
+       dmem[d0] <= [w21, w20]
+       dmem[d1] <= [w23, w22] */
+  li        x2, 20
+  la        x3, d0
+  bn.sid    x2, 0(x3++)
+  li        x2, 21
+  bn.sid    x2++, 0(x3)
+  la        x3, d1
+  bn.sid    x2, 0(x3++)
+  li        x2, 23
+  bn.sid    x2, 0(x3)
 
   ret
 
