c8f6fdc24832c38dce3d853f539fee5cbc3bb0ac
Abdullah Varici <abdullah.varici@lowrisc.org>
2023-04-18 13:07:25 +0100

    Change destination registers to remove leakages.

--- sw/otbn/crypto/p256_key_from_seed_sca.s
+++ sw/otbn/crypto/p256_key_from_seed_sca.s
@@ -59,9 +59,10 @@ run_gen_secret_key:
   bn.lid    x2, 0(x3++)
   li        x2, 21
   bn.lid    x2++, 0(x3)
+  li        x2, 10
   la        x3, seed1
   bn.lid    x2, 0(x3++)
-  li        x2, 23
+  li        x2, 11
   bn.lid    x2, 0(x3)
 
 #  # overwrite shares in DMEM with random values
@@ -87,18 +88,18 @@ run_gen_secret_key:
        [w23,w33] <= d1 */
   jal       x1, p256_key_from_seed
 
-  /* Write the results to DMEM.
-       dmem[d0] <= [w21, w20]
-       dmem[d1] <= [w23, w22] */
-  li        x2, 20
-  la        x3, d0
-  bn.sid    x2, 0(x3++)
-  li        x2, 21
-  bn.sid    x2++, 0(x3)
-  la        x3, d1
-  bn.sid    x2, 0(x3++)
-  li        x2, 23
-  bn.sid    x2, 0(x3)
+#  /* Write the results to DMEM.
+#       dmem[d0] <= [w21, w20]
+#       dmem[d1] <= [w23, w22] */
+#  li        x2, 20
+#  la        x3, d0
+#  bn.sid    x2, 0(x3++)
+#  li        x2, 21
+#  bn.sid    x2++, 0(x3)
+#  la        x3, d1
+#  bn.sid    x2, 0(x3++)
+#  li        x2, 23
+#  bn.sid    x2, 0(x3)
 
   ret
 
